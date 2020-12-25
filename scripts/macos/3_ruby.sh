a7cd4302f0684130e08df21c617f752516089cb0
tomholford <tomholford@users.noreply.github.com>
2020-12-25 13:25:50 -0800

    use ruby 3.0.0

--- scripts/macos/3_ruby.sh
+++ scripts/macos/3_ruby.sh
@@ -2,7 +2,7 @@
 
 echo "Installing ruby..."
 rbenv install 2.7.2
-rbenv install 3.0.0-preview1 
-rbenv global 3.0.0-preview1
+rbenv install 3.0.0 
+rbenv global 3.0.0
 
 gem install bundler foreman middleman rails irbtools amazing_print pry
