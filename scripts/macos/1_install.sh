51c4fd40874e8171026c9b47f6cbb32f909ccea5
tomholford <tomholford@users.noreply.github.com>
2021-03-09 22:24:39 -0800

    install zsh autosuggestions

--- scripts/macos/1_install.sh
+++ scripts/macos/1_install.sh
@@ -101,9 +101,9 @@ mysides add Documents file:///Users/tt/Documents
 mysides add Downloads file:///Users/tt/Downloads
 mysides add Pictures file:///Users/tt/Pictures
 
-# TODO: prefer oh-my-zsh
 echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo "Installing node..."
 nvm install node
